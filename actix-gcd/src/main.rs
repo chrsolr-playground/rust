908ffd945e281ec02ffba63079d581544456c2ba
chrsolr <chr.solr@gmail.com>
2023-04-06 21:44:16 -0500

    Finished actix-gcd

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,45 +1,67 @@
-use actix_web::{ web, App, HttpServer, HttpResponse };
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 
+#[derive(Deserialize)]
 struct GcdParameters {
     n: u64,
     m: u64,
 }
- 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
             .route("/gcd", web::post().to(post_index))
-    }).bind(("127.0.0.1", 3000))
-        .expect("ERROR 1")
-        .run()
-        .await
+    })
+    .bind(("127.0.0.1", 3000))
+    .expect("Server Error...")
+    .run()
+    .await
 }
 
 async fn get_index() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(
-            r#"
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
             <title>GCD Calculator</title>
             <form action="/gcd" method="post">
               <input type="text" name="n">
-              <input type="text" name="n">
+              <input type="text" name="m">
               <button type="submit">Compute GCD</button>
             </form>
             "#,
-        )
+    )
 }
 
 async fn post_index(form: web::Form<GcdParameters>) -> HttpResponse {
     if form.n == 0 || form.m == 0 {
         return HttpResponse::BadRequest()
             .content_type("text/html")
-            .body("COCOCOCOCOOD");
+            .body("Computing the GCD with zero is boring.");
     }
 
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body("damdso")
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} is <b>{}</b>.\n",
+        form.n,
+        form.m,
+        gcd(form.n, form.m),
+    );
+
+    HttpResponse::Ok().content_type("text/html").body(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    loop {
+        if m == 0 {
+            break;
+        }
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
